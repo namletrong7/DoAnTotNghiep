ac4a21cb0232419c3c078df40b2ca7d9a2974c86
Le Trong Nam <namletrong7@gmail.com>
2024-05-06 23:47:42 +0700

    thêm api đk token thông báo

--- ProjectDoAn/src/api/Api.ts
+++ ProjectDoAn/src/api/Api.ts
@@ -10,7 +10,7 @@ const Api = (isFormData:boolean) => {
     //Hàm tạo header
     const apiConfig = () => {
         return axios.create({
-            baseURL: "http://192.168.1.108:8080/DOAN/",
+            baseURL: "http://3.25.188.2/DOAN/",
             headers: {
                 'Content-Type': isFormData?'multipart/form-data':'application/json',
                 // Thêm các headers khác nếu cần thiết
