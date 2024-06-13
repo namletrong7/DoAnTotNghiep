8f042576a08a1382939a45f8652591e4fa6d7d61
namltc <namltc.bkav@gmail.com>
2024-06-13 11:21:00 +0700

    theem chuc nang loa mor task trong project

--- ProjectDoAn/src/screens/detalTaskScreen/DetailTaskScreen.js
+++ ProjectDoAn/src/screens/detalTaskScreen/DetailTaskScreen.js
@@ -222,7 +222,7 @@ export const DetailTaskScreen = React.memo(({navigation,route})=>{
 
   return (
     <Animated.View
-      entering={SlideInRight.duration(1000)} exiting={SlideOutLeft.duration(500)}
+      entering={SlideInRight.duration(400)} exiting={SlideOutLeft.duration(500)}
       style={{ flex: 1}}
     >
     <LinearGradient  colors={['#e3efdd', '#faeeca', '#deedda']} style={{height:"100%",backgroundColor:"#EEEEEE"}}>
